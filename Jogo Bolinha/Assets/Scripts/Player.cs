11ad078c708bbeb63c9d29673d32a864a3143aca
Nands25 <fernanda.araujo1@escolar.ifrn.edu.br>
2023-10-09 14:54:52 -0300

    video 4

--- Jogo Bolinha/Assets/Scripts/Player.cs
+++ Jogo Bolinha/Assets/Scripts/Player.cs
@@ -6,15 +6,26 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
-
+    public int forcaPulo = 7;
+    public bool noChao; 
     private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     { 
         Debug.Log(message: "START");
         TryGetComponent(out rb);
+        
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChao && collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -22,10 +33,15 @@ public class Player : MonoBehaviour
         
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
         Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao*velocidade*Time.deltaTime,ForceMode.Impulse);
 
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo,ForceMode.Impulse);
+            noChao = false;
+        }
+        
         if (transform.position.y <= -10)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
